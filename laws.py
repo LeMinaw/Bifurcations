44dad1f4ae2f6e233e27917b5ae3a9e6a282c1d3
LeMinaw <leminaw@gmail.com>
2018-02-14 06:47:14 +0100

    Refactor of population grow rates models

--- laws.py
+++ laws.py
@@ -7,14 +7,28 @@ Each function takes 3 args:
 from utils import clamp
 from math  import sin
 
-def logistic(pop, fertility, environment=0):
-    """Returns the next rank of the discrete logistic function."""
+def logistic(pop=.5, fertility=1, environment=0):
+    """Logistic equation.
+    fp(1-p) = -fp^2 + fp"""
+    return fertility * pop * (1 - pop)
+
+def logistic_env_product(pop=.5, fertility=1, environment=0):
+    """Tweaked version of the logistic function, taking environment into account by multiplicating population.
+     fp(1-p)e = -fep^2 + fep"""
+    return logistic(pop, fertility) * environment
+    # return fertility * pop * (1 - pop) * environment
+
+def logistic_env_fert(pop=.5, fertility=1, environment=0):
+    """Tweaked version of the logistic function, taking environment into account by modifying fertility.
+    (f+e)p(1-p) = -(f+e)p^2 + (f+e)p"""
+    return logistic(pop, fertility + environment)
+    # return (fertility + environment) * pop * (1 - pop)
+
+def lotka_volterra(pop=.5, fertility=1, environment=0):
+    """Competitive Lotka–Volterra equation.
+    fp(1-p+e) = -fp^2 + f(1+e)p"""
     return fertility * pop * (1 - pop + environment)
 
-def logistic_clamp(pop, fertility, environment=0):
-    """Clamped version of the logistic function."""
-    return clamp(fertility * pop * (1 - pop + environment))
-
-def trigonometric(pop, fertility, environment=0):
+def trigonometric(pop=.5, fertility=1, environment=0):
     """Returns the next rank of a strange sin based function."""
     return fertility * (sin(pop + environment) + 1) / 5
